f54d483e77c87ae423ce148e9593546f6aa2c6c7
5j9 <5j9@users.noreply.github.com>
2026-03-06 09:47:41 +0330

    feat: skip if mime_data.hasUrls()
    will skip file names. if desired, use f2 go into rename mode and
    copy the file name only rather than the whole file.

--- cliptalk/qt_server.py
+++ cliptalk/qt_server.py
@@ -78,7 +78,7 @@ def on_clipboard_changed():
     mime_data = clipboard.mimeData()
     assert mime_data is not None
     # Only process if the clipboard contains text
-    if not mime_data.hasText():
+    if mime_data.hasUrls() or not mime_data.hasText():
         return
 
     text = rm_urls(mime_data.text()).strip().replace('#', '').replace('*', '')
